de4b2bc8e292a18997607e705efb723852e0d198
Naga_Sai <ccnagasai2@gmail.com>
2024-04-01 01:52:53 +0530

    added changes in the project like html files, forms, views,models

--- employee_project/employee_register/models.py
+++ employee_project/employee_register/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Position(models.Model):
+    title = models.CharField(max_length=25)
+
+class Employee(models.Model):
+    Full_Name = models.CharField(max_length=50)
+    Emp_code = models.CharField(max_length=3)
+    Mobile_No = models.CharField(max_length=13)
+    position = models.ForeignKey(Position,on_delete=models.CASCADE)
+
